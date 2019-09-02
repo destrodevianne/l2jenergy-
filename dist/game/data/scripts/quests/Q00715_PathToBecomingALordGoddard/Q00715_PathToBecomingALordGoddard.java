788206267643cb73d732669f41983cb2938bbbc4
Мо3олЬ <vetal6666i@gmail.com>
2019-09-02 23:22:11 +0300

    фикс и локализация Q00715

--- dist/game/data/scripts/quests/Q00715_PathToBecomingALordGoddard/Q00715_PathToBecomingALordGoddard.java
+++ dist/game/data/scripts/quests/Q00715_PathToBecomingALordGoddard/Q00715_PathToBecomingALordGoddard.java
@@ -22,178 +22,247 @@ import com.l2jserver.gameserver.instancemanager.CastleManager;
 import com.l2jserver.gameserver.instancemanager.FortManager;
 import com.l2jserver.gameserver.model.actor.L2Npc;
 import com.l2jserver.gameserver.model.actor.instance.L2PcInstance;
-import com.l2jserver.gameserver.model.entity.Castle;
 import com.l2jserver.gameserver.model.entity.Fort;
 import com.l2jserver.gameserver.model.quest.Quest;
 import com.l2jserver.gameserver.model.quest.QuestState;
+import com.l2jserver.gameserver.model.quest.State;
 import com.l2jserver.gameserver.network.NpcStringId;
 import com.l2jserver.gameserver.network.clientpackets.Say2;
 import com.l2jserver.gameserver.network.serverpackets.NpcSay;
 
-public class Q00715_PathToBecomingALordGoddard extends Quest
+/**
+ * Path to Becoming a Lord - Goddard (715)
+ * @author Pandragon
+ */
+public final class Q00715_PathToBecomingALordGoddard extends Quest
 {
-	private static final int Alfred = 35363;
-	
-	private static final int WaterSpiritAshutar = 25316;
-	private static final int FireSpiritNastron = 25306;
-	
-	private static final int GoddardCastle = 7;
+	// NPC
+	private static final int ALFRED = 35363;
+	// Monsters
+	private static final int WATER_SPIRIT = 25316;
+	private static final int FLAME_SPIRIT = 25306;
+	// Misc
+	private static final int CASTLE_ID = 7; // Goddard
 	
 	public Q00715_PathToBecomingALordGoddard()
 	{
-		super(715, Q00715_PathToBecomingALordGoddard.class.getSimpleName(), "Path To Becoming A Lord Goddard");
-		addStartNpc(Alfred);
-		addTalkId(Alfred);
-		addKillId(WaterSpiritAshutar);
-		addKillId(FireSpiritNastron);
+		super(715, Q00715_PathToBecomingALordGoddard.class.getSimpleName(), "Path to Becoming a Lord - Goddard");
+		addStartNpc(ALFRED);
+		addTalkId(ALFRED);
+		addKillId(WATER_SPIRIT, FLAME_SPIRIT);
 	}
 	
 	@Override
 	public String onAdvEvent(String event, L2Npc npc, L2PcInstance player)
 	{
-		final QuestState qs = player.getQuestState(getName());
-		final Castle castle = CastleManager.getInstance().getCastleById(GoddardCastle);
-		if (castle.getOwner() == null)
+		final QuestState qs = getQuestState(player, false);
+		String htmltext = null;
+		if (qs == null)
 		{
-			return "Castle has no lord";
+			return htmltext;
 		}
 		
-		if (event.equals("alfred_q715_03.htm"))
+		switch (event)
 		{
-			qs.startQuest();
-		}
-		else if (event.equals("alfred_q715_04a.htm"))
-		{
-			qs.setCond(3);
-		}
-		else if (event.equals("alfred_q715_04b.htm"))
-		{
-			qs.setCond(2);
-		}
-		else if (event.equals("alfred_q715_08.htm"))
-		{
-			if (castle.getOwner().getLeader().getPlayerInstance() != null)
+			case "35363-02.html":
 			{
-				final NpcSay packet = new NpcSay(npc.getObjectId(), Say2.NPC_SHOUT, npc.getId(), NpcStringId.S1_HAS_BECOME_THE_LORD_OF_THE_TOWN_OF_GODDARD_MAY_THERE_BE_GLORY_IN_THE_TERRITORY_OF_GODDARD);
-				packet.addStringParameter(player.getName());
-				npc.broadcastPacket(packet);
-				
-				/**
-				 * Territory terr = TerritoryWarManager.getInstance().getTerritory(castle.getId()); terr.setLordId(castleOwner.getObjectId()); terr.updateDataInDB(); terr.updateState();
-				 */
-				
-				qs.exitQuest(true, true);
+				htmltext = event;
+				break;
 			}
-		}
-		return event;
-	}
-	
-	@Override
-	public String onTalk(L2Npc npc, L2PcInstance player)
-	{
-		final QuestState qs = getQuestState(player, true);
-		String htmltext = getNoQuestMsg(player);
-		final Castle castle = CastleManager.getInstance().getCastleById(GoddardCastle);
-		if (castle.getOwner() == null)
-		{
-			return "Castle has no lord";
-		}
-		final L2PcInstance castleOwner = castle.getOwner().getLeader().getPlayerInstance();
-		
-		if (qs.isCond(0))
-		{
-			if (castleOwner == qs.getPlayer())
+			case "35363-04a.html":
 			{
-				if (!hasFort())
+				if (qs.isCreated())
 				{
-					htmltext = "alfred_q715_01.htm";
+					qs.startQuest();
+					htmltext = event;
 				}
-				else
-				{
-					htmltext = "alfred_q715_00.htm";
-					qs.exitQuest(true);
-				}
+				break;
 			}
-			else
+			case "35363-05.html":
 			{
-				htmltext = "alfred_q715_00a.htm";
-				qs.exitQuest(true);
+				if (qs.isCond(1))
+				{
+					qs.setCond(2);
+					htmltext = event;
+				}
+				break;
 			}
-		}
-		else if (qs.isCond(1))
-		{
-			htmltext = "alfred_q715_03.htm";
-		}
-		else if (qs.isCond(2))
-		{
-			htmltext = "alfred_q715_05b.htm";
-		}
-		else if (qs.isCond(3))
-		{
-			htmltext = "alfred_q715_05a.htm";
-		}
-		else if (qs.isCond(4))
-		{
-			qs.setCond(6);
-			htmltext = "alfred_q715_06b.htm";
-		}
-		else if (qs.isCond(5))
-		{
-			qs.setCond(7);
-			htmltext = "alfred_q715_06a.htm";
-		}
-		else if (qs.isCond(6))
-		{
-			htmltext = "alfred_q715_06b.htm";
-		}
-		else if (qs.isCond(7))
-		{
-			htmltext = "alfred_q715_06a.htm";
-		}
-		else if (qs.isCond(8) || qs.isCond(9))
-		{
-			htmltext = "alfred_q715_07.htm";
+			case "35363-06.html":
+			{
+				if (qs.isCond(1))
+				{
+					qs.setCond(3);
+					htmltext = event;
+				}
+				break;
+			}
+			case "35363-12.html":
+			{
+				if (qs.isCond(7))
+				{
+					if (CastleManager.getInstance().getCastleById(CASTLE_ID).getSiege().isInProgress())
+					{
+						return "35363-11a.html";
+					}
+					
+					for (Fort fort : FortManager.getInstance().getForts())
+					{
+						if (!fort.isBorderFortress() && fort.getSiege().isInProgress())
+						{
+							return "35363-11a.html";
+						}
+						else if (!fort.isBorderFortress() && (fort.getContractedCastleId() != CASTLE_ID))
+						{
+							return "35363-11b.html";
+						}
+					}
+					
+					// DeclareLord(godad_dominion, player);
+					CastleManager.getInstance().getCastleById(CASTLE_ID).setShowNpcCrest(true);
+					NpcSay packet = new NpcSay(npc.getObjectId(), Say2.NPC_SHOUT, npc.getId(), NpcStringId.S1_HAS_BECOME_THE_LORD_OF_THE_TOWN_OF_GODDARD_MAY_THERE_BE_GLORY_IN_THE_TERRITORY_OF_GODDARD);
+					packet.addStringParameter(player.getName());
+					npc.broadcastPacket(packet);
+					qs.exitQuest(false, true);
+					htmltext = event;
+				}
+				break;
+			}
 		}
 		return htmltext;
 	}
 	
 	@Override
-	public final String onKill(L2Npc npc, L2PcInstance player, boolean isPet)
+	public String onTalk(L2Npc npc, L2PcInstance talker)
 	{
-		final QuestState qs = player.getQuestState(getName());
-		if (qs == null)
+		final QuestState qs = getQuestState(talker, true);
+		String htmltext = getNoQuestMsg(talker);
+		
+		if ((talker.getClan() == null) || (talker.getClan().getLeaderId() != CastleManager.getInstance().getCastleById(CASTLE_ID).getOwnerId()))
 		{
-			return null;
-		}
-		if (qs.isCond(2) && (npc.getId() == FireSpiritNastron))
-		{
-			qs.setCond(4);
-		}
-		else if (qs.isCond(3) && (npc.getId() == WaterSpiritAshutar))
-		{
-			qs.setCond(5);
+			return htmltext;
 		}
 		
-		if (qs.isCond(6) && (npc.getId() == WaterSpiritAshutar))
+		switch (qs.getState())
 		{
-			qs.setCond(9);
-		}
-		else if (qs.isCond(7) && (npc.getId() == FireSpiritNastron))
-		{
-			qs.setCond(8);
-		}
-		return null;
-	}
-	
-	private boolean hasFort()
-	{
-		for (Fort fortress : FortManager.getInstance().getForts())
-		{
-			if (fortress.getContractedCastleId() == GoddardCastle)
+			case State.CREATED:
 			{
-				return true;
+				htmltext = (talker.getId() == talker.getClan().getLeaderId()) ? "35363-01.html" : "35363-03.html";
+				break;
+			}
+			case State.STARTED:
+			{
+				switch (qs.getCond())
+				{
+					case 1:
+					{
+						htmltext = "35363-04a.html";
+						break;
+					}
+					case 2:
+					{
+						htmltext = "35363-07.html";
+						break;
+					}
+					case 3:
+					{
+						htmltext = "35363-08.html";
+						break;
+					}
+					case 4:
+					{
+						htmltext = "35363-09.html";
+						qs.setCond(6);
+						break;
+					}
+					case 5:
+					{
+						htmltext = "35363-10.html";
+						qs.setCond(7);
+						break;
+					}
+					case 6:
+					{
+						htmltext = "35363-09.html";
+						break;
+					}
+					case 7:
+					{
+						htmltext = "35363-10.html";
+						break;
+					}
+					case 8:
+					case 9:
+					{
+						htmltext = "35363-11.html";
+						break;
+					}
+				}
+			}
+			case State.COMPLETED:
+			{
+				htmltext = getAlreadyCompletedMsg(talker);
+				break;
 			}
 		}
-		return false;
+		return htmltext;
+	}
+	
+	@Override
+	public String onKill(L2Npc npc, L2PcInstance killer, boolean isSummon)
+	{
+		if (killer.getClan() == null)
+		{
+			return super.onKill(npc, killer, isSummon);
+		}
+		
+		L2PcInstance leader = killer.getClan().getLeader().getPlayerInstance();
+		if (!leader.isOnline())
+		{
+			return super.onKill(npc, killer, isSummon);
+		}
+		
+		QuestState qs = leader.getQuestState(getName());
+		if (qs == null)
+		{
+			return super.onKill(npc, killer, isSummon);
+		}
+		
+		switch (qs.getCond())
+		{
+			case 2:
+			{
+				if (npc.getId() == FLAME_SPIRIT)
+				{
+					qs.setCond(4);
+				}
+				break;
+			}
+			case 3:
+			{
+				if (npc.getId() == WATER_SPIRIT)
+				{
+					qs.setCond(5);
+				}
+				break;
+			}
+			case 6:
+			{
+				if (npc.getId() == WATER_SPIRIT)
+				{
+					qs.setCond(9);
+				}
+				break;
+			}
+			case 7:
+			{
+				if (npc.getId() == FLAME_SPIRIT)
+				{
+					qs.setCond(8);
+				}
+				break;
+			}
+		}
+		
+		return super.onKill(npc, killer, isSummon);
 	}
 }
