3f2bb87f354c346b36d0e7f2b97943daba69a1f6
Мо3олЬ <vetal6666i@gmail.com>
2019-09-02 11:32:00 +0000

    Update pccafe_coupons.sql

--- dist/tools/sql/game/pccafe_coupons.sql
+++ dist/tools/sql/game/pccafe_coupons.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `pccafe_coupons` (
-  `used_by` varchar(255) NOT NULL,
-  `coupon_use` INT UNSIGNED NOT NULL DEFAULT 0,
-  `coupon_value` int(10) unsigned NOT NULL DEFAULT 0,
-  `serial_code` text NOT NULL,
-  PRIMARY KEY (`coupon_use`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+`serial_code` VARCHAR(20) NOT NULL DEFAULT '',
+`coupon_use` INT NOT NULL DEFAULT '0',
+`coupon_value`  INT NOT NULL DEFAULT '0',
+`used_by` VARCHAR(35) CHARACTER SET UTF8 NOT NULL DEFAULT '',
+PRIMARY KEY (serial_code)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
