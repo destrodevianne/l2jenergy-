cb62d1b7d733eabdc3e8ef3b8f64315176597b59
Мо3олЬ <3@10.212.182.216>
2020-05-06 16:14:05 +0300

    Do not save arrows based on GameTimeController calculation.

--- java/com/l2jserver/gameserver/model/actor/instance/L2PcInstance.java
+++ java/com/l2jserver/gameserver/model/actor/instance/L2PcInstance.java
@@ -6369,11 +6369,6 @@ public class L2PcInstance extends L2Playable
 				arrows.changeCountWithoutTrace(-1, this, null);
 				arrows.setLastChange(L2ItemInstance.MODIFIED);
 				
-				// could do also without saving, but let's save approx 1 of 10
-				if ((GameTimeController.getInstance().getGameTicks() % 10) == 0)
-				{
-					arrows.updateDatabase();
-				}
 				_inventory.refreshWeight();
 			}
 		}
